1ba29cbde089b7bb0de181462291026d48d6cd61
Dipanjan Kundu <dipanjankundu@Dipanjant-Kundu.local>
2023-03-05 19:33:44 +0530

    initial BDD framework set up is done

--- Utilities/configReader.py
+++ Utilities/configReader.py
@@ -8,5 +8,5 @@ from configparser import ConfigParser
 
 def readConfig(section,key):
     config = ConfigParser()
-    config.read("./ConfigurationData/conf.ini")
+    config.read("./configurationdata/conf.ini")
     return config.get(section,key)
