1608804a9ae1cde0fe2006a862bd7e6f617fe750
Aditya Joshi <aditya.joshi>
2023-04-12 20:48:59 +0530

    Ignoring allure report generation logs

--- entrypoint_ui_automation.sh
+++ entrypoint_ui_automation.sh
@@ -5,7 +5,5 @@ echo "URL: " $URL
 echo "Yeppee!!!....... UI Automation Started Started in $BROWSER......."
 #source venv/bin/activate
 behave features -D url=$URL -D browser=$BROWSER -D headless=true -f allure_behave.formatter:AllureFormatter -o reports/json_reports/
-allure generate reports/json_reports -o reports/allure_reports --clean
+allure generate reports/json_reports -o reports/allure_reports --clean > /dev/null
 python3 -m utilities.mail_util
-
-
